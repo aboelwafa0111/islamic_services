3deec53a61410fd8dc16b8f92137dcb792c8ba3f
ahmed abo el wafa <ahmed.aboelwafa@beeieg.com>
2019-05-06 13:29:00 +0200

    add landing 2

---
+++ landing_2.php
@@ -0,0 +1,54 @@
+<!-- ==================================== -->
+<?php include 'header.php';?> 
+<!-- ==================================== -->
+<style type="text/css">
+	.header-clear-medium {
+    padding-top: 30px;
+}
+</style>
+<div class="header header-fixed header-logo-center">
+<a href="index.html" class="header-title">خدمات اسلامية</a>
+<a href="#" class="back-button header-icon header-icon-1"><i class="fas fa-arrow-left"></i></a>
+<a href="#" data-toggle-theme-switch class="header-icon header-icon-4"><i class="fas fa-lightbulb"></i></a>
+</div>
+
+
+<div class="page-content header-clear-medium">
+
+
+<div data-height="130" class="caption caption-margins round-medium shadow-huge">
+<div class="caption-center right-15 text-right">
+<p class="under-heading color-white opacity-90 bottom-0">
+رقم التليفون
+</p>
+<h3 class="color-white bolder">0123456789</h3>
+</div>
+<div class="caption-overlay bg-black opacity-70"></div>
+<div class="caption-bg bg-18"></div>
+</div>
+
+<div data-height="130" class="caption caption-margins round-medium shadow-huge">
+<div class="caption-center right-15 text-right">
+<h3 class="color-white bolder">تاريخ التفعيل : <Span> 1 / 5 / 2019</Span></h3>
+<h3 class="color-white bolder">تاريخ التجديد : <Span> 1 / 6 / 2019</Span></h3>
+</div>
+<div class="caption-overlay bg-black opacity-70"></div>
+<div class="caption-bg bg-21"></div>
+</div>
+
+
+<div class="content-boxed">
+<div class="content right-15 text-right">
+<h4 class="bold">إلغاء الاشتراك</h4>
+<p class="bottom-10">
+هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في الصفحة التي يقرأها
+</p>
+</div>
+</div>
+
+<div class="divider divider-margins"></div>
+</div>
+
+<!-- ==================================== -->
+<?php include 'footer.php';?> 
+<!-- ==================================== -->
