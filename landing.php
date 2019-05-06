669f561cdbe59a571f12dcefec72c29770ec4d2d
ahmed abo el wafa <ahmed.aboelwafa@beeieg.com>
2019-05-06 12:59:59 +0200

    landing

---
+++ landing.php
@@ -0,0 +1,85 @@
+
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover" />
+<title>خدمات اسلامية</title>
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900,900i|Source+Sans+Pro:300,300i,400,400i,600,600i,700,700i,900,900i" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="styles/style.css">
+<link rel="stylesheet" type="text/css" href="styles/framework.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="styles/custom.css">
+</head>
+<body class="theme-dark" data-background="none" data-highlight="green1">
+<div id="page">
+<div id="page-preloader">
+<div class="loader-main"><div class="preload-spinner border-highlight"></div></div>
+</div>
+
+<div class="page-content">
+<div class="cover-wrapper cover-has-buttons">
+<a href="home.php" class="cover-button button button-round-huge shadow-large button-full bg-green1-dark left-40 right-40 button-m scale-box">ادخل الان</a>
+<a href="#" class="cover-next color-gray2-dark">التالى<i class="fa fa-angle-right left-10"></i></a>
+<a href="#" class="cover-prev color-gray2-dark"><i class="fa fa-angle-left right-10"></i>السابق</a>
+<div class="cover-slider owl-carousel">
+<div data-height="cover" class="caption">
+<div class="caption-top caption-height-70">
+<div class="caption-overlay bg-gradient-fade"></div>
+<div class="caption-bg bg-18"></div>
+</div>
+<div class="caption-bottom bottom-100">
+<h1 class="center-text bolder font-33 bottom-0">خدمات اسلامية</h1>
+<h6 class="center-text thiner font-15 bottom-20 color-highlight">لوريم إيبسوم هو ببساطة نص شكلي</h6>
+<p class="color-theme boxed-text-huge opacity-80 bottom-50">
+هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في الصفحة التي يقرأها
+</p>
+</div>
+</div>
+<div data-height="cover" class="caption">
+<div class="caption-top caption-height-70">
+<div class="caption-overlay bg-gradient-fade"></div>
+<div class="caption-bg bg-20"></div>
+</div>
+<div class="caption-bottom bottom-100">
+<h1 class="center-text bolder font-33 bottom-0">خدمات اسلامية</h1>
+<h6 class="center-text thiner font-15 bottom-20 color-highlight">لوريم إيبسوم هو ببساطة نص شكلي</h6>
+<p class="color-theme boxed-text-huge opacity-80 bottom-50">
+هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في الصفحة التي يقرأها
+</p>
+</div>
+</div>
+<div data-height="cover" class="caption">
+<div class="caption-top caption-height-70">
+<div class="caption-overlay bg-gradient-fade"></div>
+<div class="caption-bg bg-21"></div>
+</div>
+<div class="caption-bottom bottom-100">
+<h1 class="center-text bolder font-33 bottom-0">خدمات اسلامية</h1>
+<h6 class="center-text thiner font-15 bottom-20 color-highlight">لوريم إيبسوم هو ببساطة نص شكلي</h6>
+<p class="color-theme boxed-text-huge opacity-80 bottom-50">
+هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في الصفحة التي يقرأها
+</p>
+</div>
+</div>
+<div data-height="cover" class="caption">
+<div class="caption-top caption-height-70">
+<div class="caption-overlay bg-gradient-fade"></div>
+<div class="caption-bg bg-28"></div>
+</div>
+<div class="caption-bottom bottom-100">
+<h1 class="center-text bolder font-33 bottom-0">خدمات اسلامية</h1>
+<h6 class="center-text thiner font-15 bottom-20 color-highlight">لوريم إيبسوم هو ببساطة نص شكلي</h6>
+<p class="color-theme boxed-text-huge opacity-80 bottom-50">
+هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في الصفحة التي يقرأها
+</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<!-- ==================================== -->
+<?php include 'footer.php';?> 
+<!-- ==================================== -->
